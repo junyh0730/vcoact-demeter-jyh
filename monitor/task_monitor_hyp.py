2c6e7ae7edf53b013602e0087b1338f606f44c96
gspark <gspark@dgist.ac.kr>
2022-09-08 10:09:50 +0900

    rm merge log

--- monitor/task_monitor_hyp.py
+++ monitor/task_monitor_hyp.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:monitor/task_monitor_hyp.py
 class TaskMonitorHPY():
-=======
-class TaskMonitorinHPY():
->>>>>>> 5def005d4884d6d40c2633198dbbbdeef3a7a95c:monitor/task_monitor_in_hyp.py
     def __init__(self,env):
         return
     

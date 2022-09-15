4426a88a0537f3670df577cb3bad5949b4e2a7da
gspark <gspark@dgist.ac.kr>
2022-09-15 17:50:36 +0900

    add temp. func of actor in vm. we have to test it working well

--- actor/actor_vm.py
+++ actor/actor_vm.py
@@ -1,3 +1,4 @@
+from cgroupspy import trees
 import subprocess
 import os
 
@@ -11,12 +12,46 @@ class ActorVM():
 
         self.cur_t_core = {'start':0, 'end':int(env.max_core - 1)}
         self.cur_vq_core = {'start':0, 'end':int(env.max_core/2 - 1)}
+
         self.l_fd_xps = list()
+        self.ll_fd_irq_aff = list()
+        self.ll_fd_irq_aff_list = list()
 
         self.__open_fd_xps()
-
+        self.__open_fd_irq()
+        self.__init_cset_ctrl()
         self.__init_actor()
         return
+    
+    def act(self,action):
+        return
+    
+    def __open_fd_irq(self):
+        for name in ["input", "output"]:
+            grep_cmd = "IRQ_NUM_LIST=`cat /proc/interrupts | grep " + str(name) + " | awk -F: '{print $1}'`"
+            irqs = subprocess.check_output(grep_cmd,shell=True)
+            l_irq = irqs.decode('utf-8').split()
+
+            l_fd_irq_aff = list()
+            l_fd_irq_aff_list = list()
+
+            for irq in l_irq:
+                fd_irq_aff = os.open("/proc/irq/"+str(irq)+"/smp_affinity")
+                fd_irq_aff_list = os.open("/proc/irq/"+str(irq)+"/smp_affinity_list")
+
+                l_fd_irq_aff.append(fd_irq_aff)
+                l_fd_irq_aff_list.append(fd_irq_aff_list)
+            
+            self.ll_fd_irq_aff.append(l_fd_irq_aff)
+            self.ll_fd_irq_aff_list.append(l_fd_irq_aff_list)
+        
+        return
+    
+    def __close_fd_irq(self):
+        for l_fd_irq_aff, l_fd_irq_aff_list in zip(self.ll_fd_irq_aff, self.ll_fd_irq_aff_list):
+            for fd_irq_aff, fd_irq_aff_list in zip(l_fd_irq_aff, l_fd_irq_aff_list):
+                os.close(fd_irq_aff)
+                os.close(fd_irq_aff_list)
     
     def __open_fd_xps(self):
         for core in range(self.env.max_core):
@@ -30,22 +65,47 @@ class ActorVM():
             os.close(fd)
         return
     
+    def __init_cset_ctrl(self):
+        t = trees.Tree()
+        cset = t.get_node_by_path('/cpuset/')
+        self.cset_ctrl = cset.controller
+        
+        return
+
+    
     def __init_actor(self):
         self.alloc_t_core(self.cur_t_core)
         self.alloc_vq_core(self.cur_vq_core)
         return
     
     def alloc_t_core(self,target):
+        self.cset_ctrl.cpus = range(target['start'], target['end'] + 1)
         return
     
     def alloc_vq_core(self,target):
-        num = target['end'] - target['start'] + 1
+        target_num = target['end'] - target['start'] + 1
 
-        #TODO: irq remmap
+        for l_fd_irq_aff, l_fd_irq_aff_list in zip(self.ll_fd_irq_aff, self.ll_fd_irq_aff_list):
+            #init
+            core = 0
+            aff = 1
+            max_aff = 1 << target_num 
+
+            for fd_irq_aff,fd_irq_aff_list in zip(l_fd_irq_aff,l_fd_irq_aff_list):
+                os.write(fd_irq_aff_list, core) 
+                os.write(fd_irq_aff, f'{aff:x}')
+                os.write(fd_irq_aff_list, core) 
+                
+                aff = (aff << 1) 
+                core += 1
+
+                if aff == max_aff:
+                    aff = 1
+                    core = 0
 
         #xps
         for i, fd in enumerate(self.l_fd_xps):
-            if i < num:
+            if i < target_num:
                 os.write(fd, self.HEX_CPUMASK_ALL)
             else:
                 os.write(fd, '00'.encode())
